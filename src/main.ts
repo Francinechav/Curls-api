a4a605487df76439e9bc2edfd5686aa62e3bc92f
Francinechav <fechavinda@gmail.com>
2025-12-01 16:00:17 +0200

    new

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as express from 'express';
 import { join } from 'path';
+import { AdminSeederService } from './seeds/admin-seeder.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,11 +11,15 @@ async function bootstrap() {
   app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
 
   // Enable CORS
-  app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:4000'],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
+app.enableCors({
+  origin: [process.env.FRONTEND_ORIGIN],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+});
+
+const seeder = app.get(AdminSeederService);
+await seeder.seedAdmin();
+
 
   await app.listen(process.env.PORT ?? 8080);
   console.log(`Server running on port ${process.env.PORT ?? 8080}`);
