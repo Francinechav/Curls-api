56aa73e157bfd3ff9f88e93453da44e0500f0ab0
Francinechav <fechavinda@gmail.com>
2025-12-04 15:29:10 +0200

    laaa

--- src/bookings/bookings.module.ts
+++ src/bookings/bookings.module.ts
@@ -7,10 +7,13 @@ import { Booking } from '../entities/booking';
 import { BlockedDate } from '../entities/blocked-date';
 import { Payment } from '../entities/payment';
 import { User } from '../entities/user';
+import { JwtModule } from '../jwt/jwt.module';
+
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Booking, BlockedDate, Payment, User]),
+    JwtModule,
   ],
   providers: [BookingsService],
   controllers: [BookingsController],
